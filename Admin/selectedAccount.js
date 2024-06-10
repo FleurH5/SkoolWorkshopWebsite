5a4654b771be47855aa6290445dd6770d98a60be
Fleur <fleurmaxyara@gmail.com>
2024-06-10 11:54:47 +0200

    alle data gelinkt met id en db naam

--- Admin/selectedAccount.js
+++ Admin/selectedAccount.js
@@ -5,8 +5,40 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(data => {
         if (data.status === 200) {
             const user = data.data[0];
-            document.querySelector('h2').innerText = user.Username;
-            document.querySelector('')
+            //upper information
+            document.getElementById('userName').innerText = user.Username;
+            document.getElementById('status').innerText = user.Status;
+            document.getElementById('userType').innerText = user.Role;
+            document.getElementById('email').innerText = user.Email;
+            // document.getElementById('workshops').innerText = user.
+            // bestaat daar nog niet in de db
+
+            //contact information
+            document.getElementById('phoneNumber').innerText = user.PhoneNumber;
+            document.getElementById('address').innerText = user.Address;
+            document.getElementById('postalcode').innerText = user.PostalCode;
+
+            //personal information
+            document.getElementById('DOB').innerText = user.Birthdate;
+            // document.getElementById('country').innerText = user.
+            // bestaat nog niet? city wel...
+            // document.getElementById('languagesSpoken').innerText = user.
+            // bestaat ook nog niet
+
+            //travel information
+            document.getElementById('publicTransit').innerText = user.UsesPublicTransit;
+            document.getElementById('license').innerText = user.HasLicense;
+            document.getElementById('car').innerText = user.HasCar;
+
+            //payment information
+            document.getElementById('salary').innerText = user.SalaryPerHourInEuro;
+            document.getElementById('bankinfo').innerText = user.BankId;
+            document.getElementById('btwnr').innerText = user.BTWNumber;
+            document.getElementById('kvknr').innerText = user.KVKNumber;
+            
+        } else {
+            console.log('Failed to get user data');
         }
     })
+    .catch(error => console.error('Error while fetching user data:', error));
 })
